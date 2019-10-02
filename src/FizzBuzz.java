2a6df5af02d55b7db2d487ef0c994d11ada0db70
thesspyy <punyanut.nong@kkumail.com>
2019-10-02 15:50:03 +0700

    Create Java Project with JUnit

---
+++ src/FizzBuzz.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzBuzz {
+
+	@Test
+	void test() {
+		int result = 1;
+		assertEquals(1,result);
+	}
+
+}
